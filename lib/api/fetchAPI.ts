264e1db22a669033ebadce17f8c19e1c6bbfedd0
Tai <htjpascal@gmail.com>
2020-05-29 23:54:44 +0200

    Add absolute path for import

---
+++ lib/api/fetchAPI.ts
@@ -0,0 +1,20 @@
+export default async function fetchAPI(query, { variables }: any = {}) { 
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/graphql`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      query,
+      variables,
+    }),
+  })
+
+  const json = await res.json()
+  if (json.errors) {
+    console.error(json.errors)
+    throw new Error('Failed to fetch API')
+  }
+
+  return json.data
+}
